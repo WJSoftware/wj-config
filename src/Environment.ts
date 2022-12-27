3fc3ace345d4821b425f332f441b57342891ceed
Ramirez Vargas, José Pablo <webJose@gmail.com>
2022-12-26 23:22:54 -0600

    fix: hasTraits and hasAnyTrait in Environment now throws for trait type mismatch

--- src/Environment.ts
+++ src/Environment.ts
@@ -1,6 +1,7 @@
-import type { EnvironmentTest, IEnvironment, IEnvironmentDefinition, Traits } from "wj-config";
+import type { EnvironmentTest, IEnvironment, IEnvironmentDefinition, Trait, Traits } from "wj-config";
 import { InvalidEnvNameError } from "./EnvConfigError.js";
 import { EnvironmentDefinition } from "./EnvironmentDefinition.js";
+import { isArray } from "./helpers.js";
 
 function ensureEnvDefinition(env: string | IEnvironmentDefinition): IEnvironmentDefinition {
     if (typeof env === 'string') {
@@ -33,7 +34,21 @@ export class Environment implements IEnvironment {
         }
     }
 
-    hasTraits(traits: Traits): boolean {
+    #normalizeTestTraits(traits: Trait | Traits): Trait | Traits {
+        if (typeof traits === 'number' && typeof this.current.traits !== 'number') {
+            throw new TypeError('Cannot test a numeric trait against string traits.');
+        }
+        if ((typeof traits === 'string' || (isArray(traits) && typeof traits[0] === 'string')) && typeof this.current.traits === 'number') {
+            throw new TypeError('Cannot test string traits against a numeric trait.');
+        }
+        if (typeof traits === 'string') {
+            traits = [traits];
+        }
+        return traits;
+    }
+
+    hasTraits(traits: Trait | Traits): boolean {
+        traits = this.#normalizeTestTraits(traits);
         const hasBitwiseTraits = (t: number) => ((this.current.traits as number) & t) === t && t > 0;
         const hasStringTraits = (t: string[]) => {
             let has = true;
@@ -45,10 +60,11 @@ export class Environment implements IEnvironment {
         if (typeof traits === "number") {
             return hasBitwiseTraits(traits);
         }
-        return hasStringTraits(traits);
+        return hasStringTraits(traits as string[]);
     }
 
-    hasAnyTrait(traits: Traits): boolean {
+    hasAnyTrait(traits: Trait | Traits): boolean {
+        traits = this.#normalizeTestTraits(traits);
         const hasAnyBitwiseTrait = (t: number) => ((this.current.traits as number) & t) > 0;
         const hasAnyStringTrait = (t: string[]) => {
             for (let x of t) {
@@ -61,6 +77,6 @@ export class Environment implements IEnvironment {
         if (typeof traits === "number") {
             return hasAnyBitwiseTrait(traits);
         }
-        return hasAnyStringTrait(traits);
+        return hasAnyStringTrait(traits as string[]);
     }
 }
