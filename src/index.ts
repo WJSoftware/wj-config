cfe345c1ac8e26db46ac113f0fa00c092dbcabc2
Ramirez Vargas, José Pablo <webJose@gmail.com>
2024-10-27 11:29:31 -0600

    refactor: Re-import Builder

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import { IBuilder } from "wj-config";
-import Builder from "./Builder.js";
+import { Builder } from "./Builder.js";
 
+export * from "./DataSource.js";
 export * from "./Environment.js";
 export * from "./EnvironmentDefinition.js";
-export * from "./DataSource.js";
 export default function wjConfig(): IBuilder {
     return new Builder();
 }
