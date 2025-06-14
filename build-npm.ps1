9d0f095af03eca0efd049db13927d776a916899f
José Pablo Ramírez Vargas <webJose@gmail.com>
2025-06-14 12:51:29 -0600

    chore(build):  Simplify build script (#85)

--- build-npm.ps1
+++ build-npm.ps1
@@ -1,92 +1,86 @@
 <#
 .SYNOPSIS
-Compiles wj-config.
+Builds wj-config.
 
 .DESCRIPTION
-Automates all of the necessary steps to compile and optionally publish the wj-config package:
+Automates the build process for the wj-config package:
 
-1. Increments the package version according to what is specified.
+1. Cleans up the previous build artifacts.
+2. Bundles the package using Rollup.
 2. Compiles the TypeScript source code and outputs it to .\out.
 3. Copies the wj-config.d.ts definition file.
-4. Copies the package.json file.
-5. Prepares the npmjs.org readme file by joining PublishNote.md and README.md.
-6. If the Publish switch is specified, performs actual publishing to the NPM public registry.
-
-NOTE:  If the Publish switch is not specified, then npm publish is run in dry-mode, just to show the potential result of publishing.
-
-Use the Verbose switch to turn on all messages.
-
-.PARAMETER VerUpgrade
-Specify a version change.  See the documentation for the command 'npm version' for detailed information.
-
-.PARAMETER PreId
-Specify the pre-release ID to use.  Common examples would be 'alpha', 'beta' or 'rc'.  See the documentation for the command 'npm version' for detailed information.
-
-.PARAMETER Publish
-Publishes the package.
-
-.PARAMETER NpmTag
-Specifies the tag to use when publishing the package.  This is useful for pre-release versions.  See the documentation for the command 'npm publish' for detailed information.
-
-It cannot be used when the package version contains a pre-release tag, and is only used when publishing.
 #>
 [CmdletBinding(SupportsShouldProcess = $true)]
 param (
-    [Parameter(Mandatory = $false)]
-    [ValidateSet("major", "minor", "patch", "premajor", "preminor", "prepatch", "prerelease")]
-    [string] $VerUpgrade,
-    [Parameter(Mandatory = $false)]
-    [string] $PreId,
-    [Parameter(Mandatory = $false)]
-    [switch] $Publish,
-    [Parameter(Mandatory = $false)]
-    [string] $NpmTag
 )
 begin {
+    function Write-Host-Highlight {
+        param (
+            [Parameter(Mandatory = $true)]
+            [string]$Message,
+            [ConsoleColor]$ForegroundColor = 'Yellow',
+            [ConsoleColor]$BackgroundColor = 'DarkBlue'
+        )
+        $origFg = $Host.UI.RawUI.ForegroundColor
+        $origBg = $Host.UI.RawUI.BackgroundColor
+        $Host.UI.RawUI.ForegroundColor = $ForegroundColor
+        $Host.UI.RawUI.BackgroundColor = $BackgroundColor
+        Write-Host $Message
+        $Host.UI.RawUI.ForegroundColor = $origFg
+        $Host.UI.RawUI.BackgroundColor = $origBg
+    }
+
+    function Write-Warning-Highlight {
+        param (
+            [Parameter(Mandatory = $true)]
+            [string]$Message,
+            [ConsoleColor]$ForegroundColor = 'Black',
+            [ConsoleColor]$BackgroundColor = 'DarkYellow'  # DarkYellow is visible on both light and dark backgrounds
+        )
+        $origFg = $Host.UI.RawUI.ForegroundColor
+        $origBg = $Host.UI.RawUI.BackgroundColor
+        $Host.UI.RawUI.ForegroundColor = $ForegroundColor
+        $Host.UI.RawUI.BackgroundColor = $BackgroundColor
+        Write-Host $Message
+        $Host.UI.RawUI.ForegroundColor = $origFg
+        $Host.UI.RawUI.BackgroundColor = $origBg
+    }
+
+    function Write-Error-Highlight {
+        param (
+            [Parameter(Mandatory = $true)]
+            [string]$Message,
+            [ConsoleColor]$ForegroundColor = 'White',
+            [ConsoleColor]$BackgroundColor = 'DarkRed'  # DarkRed is visible on both light and dark backgrounds
+        )
+        $origFg = $Host.UI.RawUI.ForegroundColor
+        $origBg = $Host.UI.RawUI.BackgroundColor
+        $Host.UI.RawUI.ForegroundColor = $ForegroundColor
+        $Host.UI.RawUI.BackgroundColor = $BackgroundColor
+        Write-Host $Message
+        $Host.UI.RawUI.ForegroundColor = $origFg
+        $Host.UI.RawUI.BackgroundColor = $origBg
+    }
+
     $ErrorActionPreference = 'Stop'
     [string] $path = Resolve-Path .\
-    if ($VerUpgrade -ne '') {
-        if ($PSCmdlet.ShouldProcess($path, "Package version increment: $VerUpgrade")) {
-            if ($PreId -ne '') {
-                npm version $VerUpgrade --preid $PreId
-            }
-            else {
-                npm version $VerUpgrade
-            }
-        }
-    }
-    else {
-        Write-Verbose "Version upgrade was not specified.  The package's version will not be modified."
-    }
     if (Test-Path .\dist) {
+        Write-Verbose "Removing old dist directory..."
         Remove-Item -Path .\dist -Recurse -Force -ErrorAction SilentlyContinue
     }
     if (Test-Path .\out) {
+        Write-Verbose "Removing old out directory..."
         Remove-Item -Path .\out -Recurse
     }
     if ($PSCmdlet.ShouldProcess($path, "Bundle package")) {
+        Write-Verbose "Bundling package using rollup..."
         npx rollup -c
     }
     if ($PSCmdlet.ShouldProcess($path, "TypeScript compilation")) {
+        Write-Verbose "Transpiling TypeScript files..."
         npx tsc
     }
+    Write-Verbose "Copying .d.ts files..."
     Copy-Item .\src\wj-config.d.ts .\out
-    [string] $publishTag = $NpmTag -eq "" ? "latest" : $NpmTag
-    if (!$Publish) {
-        Write-Output "Running npm publish in dry run mode using tag '$publishTag'."
-        npm publish --tag $publishTag --dry-run
-    }
-    elseif ($PSCmdlet.ShouldProcess($path, "Publish NPM package")) {
-        npm publish --tag $publishTag
-    }
-    elseif ($WhatIfPreference) {
-        Write-Verbose "NOTE: Running npm publish in dry run mode using tag '$publishTag' and sample data for illustration purposes only."
-        if (-not (Test-Path .\out)) {
-            New-Item -Path .\out -ItemType Directory -WhatIf:$false
-        }
-        if (-not (Test-Path .\out\test.js)) {
-            New-Item -Path .\out\test.js -ItemType File -WhatIf:$false
-        }
-        npm publish --tag $publishTag --dry-run
-    }
+    Write-Host-Highlight "Building process completed."
 }
